fcc025b9600ad82b95ba57079a077891b03a6a8e
ebrahim <azeezhibro06@gmail.com>
2022-10-31 18:01:39 +0100

    weatherapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Wether from './component/wether';
+
+function App() {
+  return (
+    <div className="App">
+      <Wether/>
+    </div>
+  );
+}
+
+export default App;
